22130bafd3fa0eb3c731661ce2a011ebcc7a5689
Chang Ki Heon <changkiheon@naver.com>
2023-02-07 22:31:14 +0900

    startapp

---
+++ myinstagram/myinstagram/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+
+class Sub(APIView):
+    def get(self, request):
+        print("Get Request")
+        return render(request, "myinstagram\main.html")
+
+    def post(self, request):
+        print("Post Request")
+        return render(request, "myinstagram\main.html")
