59115d3f621091ec15c9244025a3e6807901d209
ChangKi <changkiheon@naver.com>
2023-02-12 19:50:46 +0900

    Call Data from DB to Template

--- myinstagram/content/views.py
+++ myinstagram/content/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
-# Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') # QuerySet = select * from content_feed
+        
+        # how to see feed.content
+        """
+        for feed in feed_list:
+            print(feed.content)
+        """
+
+        return render(request, "myinstagram/main.html", context=dict(feeds=feed_list))
